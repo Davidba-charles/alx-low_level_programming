5004996d8204e96c9635fe2415712ebc651c4f00
Davidba-charles <charlesdavidba@yahoo.com>
2022-03-18 21:04:13 -0700

    6 print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - print line
+ * @n: the number of '_' characters to use
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
