76daccf6e9c40a4f88aac03595fdd1830f92ce32
Davidba-charles <charlesdavidba@yahoo.com>
2022-03-19 10:13:39 -0700

    7 print diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,35 +2,24 @@
 /**
  * more_numbers - print 10 times the numbers, from 0 to 14
  *
- * Return: always 0
+ * Return: void
  */
 void more_numbers(void)
 {
 int i;
-int j;
-int k;
-int l;
-
+int x;
+for (x = 0; x < 10; x++)
+{
 i = 0;
-l = 0;
-while (i < 10)
+while (i <= 14)
 {
-while (l <= 14)
+if (i >= 10)
 {
-if (l < 10)
-{
-k = l;
+_putchar (i / 10 + '0');
 }
-else
-{
-j = l / 10;
-k = l % 10;
-_putchar (j + '0');
-l++;
-}
+_putchar(i % 10 + '0');
 i++;
-l = 0;
-_putchar ('\n');
 }
+_putchar ('\n');
 }
 }
