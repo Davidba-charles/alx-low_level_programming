240dfac161d87969b6687e836bf982c5e3e55440
Davidba-charles <charlesdavidba@yahoo.com>
2022-03-19 18:48:05 -0700

    6 print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int k;
+for (k = 48; k <= 57; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
