910b047b750cf5b67980ebcac222605af38de887
Davidba-charles <charlesdavidba@yahoo.com>
2022-03-20 06:47:05 -0700

    102 print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,41 +6,26 @@
  */
 int main(void)
 {
-int i = 48, j = 48, k = 48, l = 49;
-while (i < 58)
+int i;
+int j;
+for (i = 0; i < 100; i++)
 {
-while (j < 58)
+for (j = i + 1; j < 100; j++)
 {
-while (k < 58)
-{
-while (l < 58)
-{
-putchar(i);
-putchar(j);
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(k);
-putchar(l);
-if (!(i == 57 &&
-j == 56 &&
-k == 57 &&
-l == 57))
+
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+
+if (i == 98 && j == 99)
 {
+break;
+}
 putchar(',');
 putchar(' ');
 }
-l++;
-}
-k++;
-l = 48;
-}
-j++;
-j = i;
-l = j + 1;
-}
-i++;
-j = 48;
-k = i;
-l = j + 1;
 }
 putchar('\n');
 return (0);
