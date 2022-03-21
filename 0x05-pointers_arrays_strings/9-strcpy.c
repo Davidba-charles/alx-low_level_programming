752d8147e5fdcd0694d36980b7759ac1f3da25ab
Davidba-charles <charlesdavidba@yahoo.com>
2022-03-21 14:23:19 -0700

    9 strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copy to another string
+ * @dest: pointer
+ * Return: numbers dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
+}
