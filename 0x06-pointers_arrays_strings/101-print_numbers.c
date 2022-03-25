84f010f6b52eb843f48a45cea15f040b00af5ea1
Davidba-charles <charlesdavidba@yahoo.com>
2022-03-25 03:20:54 -0700

    101 print numbers

--- 0x06-pointers_arrays_strings/101-print_numbers.c
+++ 0x06-pointers_arrays_strings/101-print_numbers.c
@@ -6,17 +6,26 @@
  */
 void print_number(int n)
 {
+unsigned int m, d, count;
 
-unsigned int n1;
-n1 = n;
 if (n < 0)
 {
-_putchar('-');
-n1 = -n;
+_putchar(45);
+m = n * -1;
 }
-if (n1 / 10 != 0)
+else
 {
-print_number(n1 / 10);
+m = n;
 }
-_putchar((n1 % 10) + '0');
+d = m;
+count = 1;
+while (d > 9)
+{
+d /= 10;
+count *= 10;
+}
+for (; count >= 1; count /= 10)
+{
+_putchar((m / count) % 10) + 48);
+}
 }
