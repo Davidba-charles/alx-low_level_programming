5f16b72ec40674db63fb087b55439c8e93d8d3a2
Davidba-charles <charlesdavidba@yahoo.com>
2022-04-19 02:17:52 -0700

    101 strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -4,15 +4,18 @@
  * @str: pointer to string params
  * Return: char
  */
+
 char **strtow(char *str)
 {
 int i = 0, j = 0, k = 0;
 int len = 0, count = 0;
 char **f, *col;
+
 if (!str || !*str)
 {
 return (NULL);
 }
+
 while (*(str + i))
 {
 if (*(str + i) != ' ')
@@ -24,6 +27,7 @@ count += 1;
 }
 i++;
 }
+
 if (count == 0)
 {
 return (NULL);
@@ -35,6 +39,7 @@ if (!f)
 return (NULL);
 }
 i = 0;
+
 while (*str)
 {
 while (*str == ' ' && *str)
@@ -42,13 +47,14 @@ while (*str == ' ' && *str)
 str++;
 }
 len = 0;
+
 while (*(str + len) != ' ' && *(str + len))
 {
 len += 1;
 }
-
 len += 1;
 col = malloc(sizeof(char) * len);
+
 if (!col)
 {
 for (k = j - 1; k >= 0; k--)
@@ -58,12 +64,14 @@ free(f[k]);
 free(f);
 return (NULL);
 }
+
 for (k = 0; k < (len - 1); k++)
 {
 *(col + k) = *(str++);
 }
 *(col + k) = '\0';
 *(f + j) = col;
+
 if (j < (count - 1))
 {
 j++;
