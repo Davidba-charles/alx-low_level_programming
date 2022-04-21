e8a7e4840adc1e14adbd0af56d0abbef2287ad16
Davidba-charles <charlesdavidba@yahoo.com>
2022-04-21 16:34:46 -0700

    100 first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,7 +2,7 @@
 /**
  * premain - run before main
  */
-void first(void) __attribute__ ((constructor)) premain()
+void __attribute__ ((constructor)) premain()
 {
 printf(
 "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
